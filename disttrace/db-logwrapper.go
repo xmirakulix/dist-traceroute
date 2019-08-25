448ea2e8d9fe397ee49ce32b424261993592b48c
Florian Parnigoni <florian@parnigoni.net>
2019-08-25 19:48:34 +0200

    Initial db functions

---
+++ disttrace/db-logwrapper.go
@@ -0,0 +1,161 @@
+package disttrace
+
+import (
+	"database/sql"
+	"time"
+
+	// init sqlite3
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// DB Wraps sql.DB
+type DB struct {
+	*sql.DB
+}
+
+// Tx Wraps sql.Tx
+type Tx struct {
+	*sql.Tx
+}
+
+// open creates the database connection and returns a db reference
+func openDB(dataSourceName string) (*DB, error) {
+	db, err := sql.Open("sqlite3", dataSourceName)
+	if err != nil {
+		log.Warn("DB Open: Error while opening the database connection, Error: ", err)
+		return nil, err
+	}
+	log.Debug("DB Open: Successfully opened database: ", dataSourceName)
+	return &DB{db}, nil
+}
+
+// Begin creates and returns a new transaction
+func (db *DB) Begin() (*Tx, error) {
+	tx, err := db.DB.Begin()
+	if err != nil {
+		log.Warn("DB Begin: Error while creating new transaction, Error: ", err)
+		return nil, err
+	}
+	log.Debug("DB Begin: Successfully started transaction...")
+	return &Tx{tx}, nil
+}
+
+// Commit commits a transaction to the database
+func (tx *Tx) Commit() error {
+	if err := tx.Tx.Commit(); err != nil {
+		log.Warn("DB Commit: Error while commiting the transaction, Error: ", err)
+		return err
+	}
+	log.Debug("DB Commit: Successfully commited transaction")
+	return nil
+}
+
+// Rollback aborts the transaction
+func (tx *Tx) Rollback() error {
+	if err := tx.Tx.Rollback(); err != nil {
+		log.Warn("DB Commit: Error while rolling the transaction back, Error: ", err)
+		return err
+	}
+	log.Debug("DB Commit: Successfully rolled transaction back")
+	return nil
+}
+
+// Query executes a query and returns a reference to the resultset
+func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+
+	startTime := time.Now()
+
+	rows, err := db.DB.Query(query, args...)
+	if err != nil {
+		log.Warnf("DB Query: Error while executing query <%v>, duration: %v, Error: %v", query, time.Since(startTime), err)
+		return nil, err
+	}
+
+	log.Debugf("DB Query: Successfully executed query <%v>, duration: %v", query, time.Since(startTime))
+	return rows, nil
+}
+
+// QueryRow executes a query and returns a single row
+func (db *DB) QueryRow(query string, args ...interface{}) *sql.Row {
+
+	startTime := time.Now()
+
+	row := db.DB.QueryRow(query, args...)
+
+	log.Debugf("DB QueryRow: Successfully executed query <%v>, duration: %v", query, time.Since(startTime))
+	return row
+}
+
+// Exec executes a statement and returns the result
+func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
+
+	startTime := time.Now()
+
+	result, err := db.DB.Exec(query, args...)
+	if err != nil {
+		log.Warnf("DB Exec: Error while executing statement <%v>, duration: %v, Error: %v", query, time.Since(startTime), err)
+		return nil, err
+	}
+
+	log.Debugf("DB Exec: Successfully executed statement <%v>, duration: %v", query, time.Since(startTime))
+	return result, nil
+}
+
+// Ping verifies the connection to the database and establishes it if needed
+func (db *DB) Ping() error {
+	if err := db.DB.Ping(); err != nil {
+		log.Warn("DB Ping: Error while pinging database, Error: ", err)
+		return err
+	}
+	return nil
+}
+
+// Close closes the connection to the database
+func (db *DB) Close() error {
+	if err := db.DB.Close(); err != nil {
+		log.Warn("DB Close: Error while closing connection, Error: ", err)
+		return err
+	}
+	return nil
+}
+
+// Query executes a query on the given transaction and returns a reference to the resultset
+func (tx *Tx) Query(query string, args ...interface{}) (*sql.Rows, error) {
+
+	startTime := time.Now()
+
+	rows, err := tx.Tx.Query(query, args...)
+	if err != nil {
+		log.Warnf("DB QueryTx: Error while executing query <%v>, duration: %v, Error: %v", query, time.Since(startTime), err)
+		return nil, err
+	}
+
+	log.Debugf("DB QueryTx: Successfully executed query <%v>, duration: %v", query, time.Since(startTime))
+	return rows, nil
+}
+
+// QueryRow executes a query on the given transaction and returns a single row
+func (tx *Tx) QueryRow(query string, args ...interface{}) *sql.Row {
+
+	startTime := time.Now()
+
+	row := tx.Tx.QueryRow(query, args...)
+
+	log.Debugf("DB QueryRowTx: Successfully executed query <%v>, duration: %v", query, time.Since(startTime))
+	return row
+}
+
+// Exec executes a statement on the given transaction and returns the result
+func (tx *Tx) Exec(query string, args ...interface{}) (sql.Result, error) {
+
+	startTime := time.Now()
+
+	result, err := tx.Tx.Exec(query, args...)
+	if err != nil {
+		log.Warnf("DB ExecTx: Error while executing statement <%v>, duration: %v, Error: %v", query, time.Since(startTime), err)
+		return nil, err
+	}
+
+	log.Debugf("DB ExecTx: Successfully executed statement <%v>, duration: %v", query, time.Since(startTime))
+	return result, nil
+}
---
+++ disttrace/db-schema.go
@@ -0,0 +1,155 @@
+package disttrace
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// getSchemaVersion returns the current schema version of the database
+func (db *DB) getSchemaVersion() (int, error) {
+
+	log.Debug("getSchemaVersion: Reading current schema version of database...")
+
+	// check if schema info table exists
+	var foundTable string
+
+	query := "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='t_SchemaInfo'"
+	err := db.QueryRow(query).Scan(&foundTable)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Debug("getSchemaVersion: Schema info table doesn't exist, assuming emtpy database")
+		return 0, nil
+
+	case err != nil:
+		log.Warn("getSchemaVersion: Error: Couldn't determine db version, Error: ", err)
+		return -1, errors.New("Couldn't determine db version")
+
+	case foundTable != "t_SchemaInfo":
+		// what happened?!
+		log.Warnf("getSchemaVersion: Received unexpected reply '%v' while looking for table 't_SchemaInfo', can't determine db schema version", foundTable)
+		return -1, errors.New("Couldn't determine db version")
+	}
+
+	// read db version from schema info
+	var version int
+
+	query = "SELECT nVersion FROM t_SchemaInfo"
+	if err := db.QueryRow(query).Scan(&version); err != nil {
+		log.Warn("getSchemaVersion: Error: Couldn't read db version from schemainfo, Error: ", err)
+		return -1, errors.New("Couldn't read db version from schemainfo")
+	}
+
+	log.Debug("getSchemaVersion: Read dbversion from schema info: ", version)
+	return version, nil
+}
+
+// createAndUpdateDbSchema checks if the database schema is at newest state and upgrades it if needed
+func (db *DB) createAndUpdateDbSchema() error {
+
+	var err error
+	log.Debug("createAndUpdateDbSchema: Checking if database schema needs upgrading...")
+
+	// define the schema info for all db versions
+	const maxDBVersion = 2
+	var schemaUpdate [maxDBVersion + 1][]string
+
+	schemaUpdate[0] = []string{}
+
+	schemaUpdate[1] = []string{
+		"CREATE TABLE IF NOT EXISTS t_SchemaInfo (nVersion INTEGER PRIMARY KEY)",
+		"INSERT INTO t_SchemaInfo VALUES (1)",
+	}
+
+	schemaUpdate[2] = []string{
+		`CREATE TABLE IF NOT EXISTS t_Traceroutes (
+			nTracerouteId INTEGER PRIMARY KEY AUTOINCREMENT,
+			strOriginSlave TEXT NOT NULL,
+			strDestination TEXT NOT NULL,
+			dtStart INTEGER NOT NULL,
+			strAnnotations TEXT
+			)`,
+		`CREATE TABLE IF NOT EXISTS t_Hops (
+			nHopId INTEGER PRIMARY KEY AUTOINCREMENT, 
+			nHopNumber INTEGER NOT NULL,
+			strHopAddress TEXT,
+			nPreviousHopId TEXT,
+			dDuration INTEGER,
+			strAnnotations TEXT
+			)`,
+		`UPDATE t_SchemaInfo SET nVersion = 2`,
+	}
+
+	// get current DB schema version
+	var currentDBVersion int
+	if currentDBVersion, err = db.getSchemaVersion(); err != nil {
+		log.Warn("createAndUpdateDbSchema: Error: Couldn't get db version: ", err)
+		return errors.New("Couldn't get db version")
+	}
+
+	// check if schema needs upgrading
+	if currentDBVersion >= maxDBVersion {
+		log.Infof("createAndUpdateDbSchema: Schema is at version %v, no upgrade needed", currentDBVersion)
+		return nil
+	}
+
+	log.Warnf("createAndUpdateDbSchema: Database schema needs to be upgraded, current version: %v, upgrading to: %v", currentDBVersion, maxDBVersion)
+
+	// execute upgrade commands beginning at the next version
+	for _, cmds := range schemaUpdate[currentDBVersion+1:] {
+		currentDBVersion++
+		log.Infof("createAndUpdateDbSchema: Upgrading database schema to version: %v", currentDBVersion)
+		var tx *Tx
+		if tx, err = db.Begin(); err != nil {
+			log.Warn("createAndUpdateDbSchema: Couldn't start transaction, Error: ", err)
+			return errors.New("Couldn't start transaction")
+		}
+
+		// loop through upgrade comnands
+		count := 0
+		for _, cmd := range cmds {
+			count++
+			log.Debugf("createAndUpdateDbSchema: Executing command %v of %v...", count, len(cmds))
+			// exec Update
+			if _, err := tx.Exec(cmd); err != nil {
+				log.Warnf("createAndUpdateDbSchema: Error while executing query <%v>, Error: %v", cmd, err)
+				log.Warn("createAndUpdateDbSchema: Doing rollback of transaction...")
+				// error, rollback
+				if err := tx.Rollback(); err != nil {
+					log.Warn("createAndUpdateDbSchema: Couldn't rollback transaction, Error: ", err)
+					return errors.New("Couldn't rollback transaction")
+				}
+				log.Warn("createAndUpdateDbSchema: Rollback complete")
+				return errors.New("Error while executing query")
+			}
+		}
+		if err := tx.Commit(); err != nil {
+			log.Warn("createAndUpdateDbSchema: Couldn't commit transaction, Error: ", err)
+			return errors.New("Couldn't commit transaction")
+		}
+		log.Debug("createAndUpdateDbSchema: Successfully upgraded database schema to version: ", currentDBVersion)
+	}
+
+	log.Info("createAndUpdateDbSchema: Finished upgrading the database schema. Now on version: ", currentDBVersion)
+	return nil
+}
+
+// InitDBConnectionAndUpdate initializes a connection to the database and upgrades the schema if needed
+func InitDBConnectionAndUpdate(dataSourceName string) (*DB, error) {
+	var db *DB
+	var err error
+
+	log.Debug("InitDBConnectionAndUpdate: Opening db connection to: ", dataSourceName)
+
+	if db, err = openDB(dataSourceName); err != nil {
+		log.Warn("InitDBConnectionAndUpdate: Error while opening database connection, Error: ", err)
+		return nil, errors.New("Error while opening database connection")
+	}
+
+	if err := db.createAndUpdateDbSchema(); err != nil {
+		log.Warn("InitDBConnectionAndUpdate: Error while checking database schema, Error: ", err)
+		return nil, errors.New("Error while checking database schema")
+	}
+
+	log.Debug("InitDBConnectionAndUpdate: Successfully established database connection")
+	return db, nil
+}
