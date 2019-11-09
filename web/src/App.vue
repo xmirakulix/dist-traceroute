de23180e6109163a5af5d60360f2f353ec0332cb
Florian Parnigoni <florian@parnigoni.net>
2019-11-09 16:15:47 +0100

    vuetify

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,67 @@
 <template>
   <div id="app">
-    <title>dist-traceroute Master</title>
-    <h1>dist-traceroute Master</h1>
-    <p>Hi, this is the webservice of the dist-traceroute master service.</p>
-    <p>Uptime: {{ uptime }}</p>
+    <v-app>
+      <v-app-bar app clipped-left color="primary" dark>
+        <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+        <v-toolbar-title>disttrace Webinterface</v-toolbar-title>
+      </v-app-bar>
 
-    <h2>Last results received</h2>
-    <table>
-      <tr>
-        <th>nTracerouteId</th>
-        <th>strOriginSlave</th>
-        <th>strDestination</th>
-        <th>dtStart</th>
-        <th>nHopCount</th>
-      </tr>
-      <tr>
-        <td>" + fmt.Sprintf("%v", traceID) + "</td>
-        <td>" + slaveName + "</td>
-        <td>" + destName + "</td>
-        <td>" + startTime + "</td>
-        <td title='" + detailJSON + "'>" + fmt.Sprintf("%v", hopCnt) + "</td>
-      </tr>
-    </table>
+      <v-navigation-drawer app clipped permanent expand-on-hover>
+        <v-list>
+          <v-list-item @click="doAlert('home')">
+            <v-list-item-action>
+              <v-icon>fas fa-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="doAlert('contact')">
+            <v-list-item-action>
+              <v-icon>fas fa-envelope</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-    <h2>Currently loaded master config</h2>
-    <pre>" + string(masterCfgJSON) + "</pre>
+      <v-content>
+        <h1>dist-traceroute Master</h1>
+        <p>Hi, this is the webservice of the dist-traceroute master service.</p>
+        <p>Uptime: {{ uptime }}</p>
 
-    <h2>Last transmitted slave config</h2>
-    <p>Time: " + timeSinceSlaveCfg + "</p>
-    <pre>" + lastTransmittedSlaveConfig + "</pre>
+        <h2>Last results received</h2>
+        <table>
+          <tr>
+            <th>nTracerouteId</th>
+            <th>strOriginSlave</th>
+            <th>strDestination</th>
+            <th>dtStart</th>
+            <th>nHopCount</th>
+          </tr>
+          <tr>
+            <td>" + fmt.Sprintf("%v", traceID) + "</td>
+            <td>" + slaveName + "</td>
+            <td>" + destName + "</td>
+            <td>" + startTime + "</td>
+            <td title='" + detailJSON + "'>
+              " + fmt.Sprintf("%v", hopCnt) + "
+            </td>
+          </tr>
+        </table>
 
-    <AddSlave />
+        <h2>Currently loaded master config</h2>
+        <pre>" + string(masterCfgJSON) + "</pre>
+
+        <h2>Last transmitted slave config</h2>
+        <p>Time: " + timeSinceSlaveCfg + "</p>
+        <pre>" + lastTransmittedSlaveConfig + "</pre>
+
+        <AddSlave />
+      </v-content>
+
+      <v-footer color="primary" app>
+        <span class="white--text">&copy; 2019</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
@@ -40,6 +71,11 @@ import AddSlave from "./components/AddSlave.vue";
 
 export default {
   name: "app",
+  data: function() {
+    return {
+      drawer: null
+    };
+  },
 
   components: {
     AddSlave
@@ -48,7 +84,9 @@ export default {
   computed: mapGetters(["uptime"]),
 
   methods: {
-    ...mapActions(["fetchUptime"])
+    ...mapActions(["fetchUptime"]),
+
+    doAlert: text => console.log(text)
   },
 
   created() {
@@ -57,8 +95,4 @@ export default {
 };
 </script>
 
-<style scope>
-h2 {
-  margin: 40px 0 0;
-}
-</style>
+<style scope></style>
