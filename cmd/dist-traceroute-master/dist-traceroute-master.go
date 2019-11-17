aef4c298851f388387cde12ff79d2c907943d9eb
Florian Parnigoni <florian@parnigoni.net>
2019-11-17 19:48:23 +0100

    extract http server

--- cmd/dist-traceroute-master/dist-traceroute-master.go
+++ cmd/dist-traceroute-master/dist-traceroute-master.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"flag"
-	"io"
-	"net/http"
 	"os"
-	"time"
 
-	ghandlers "github.com/gorilla/handlers"
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/negroni"
 	"github.com/xmirakulix/dist-traceroute/disttrace"
 )
 
@@ -25,81 +19,7 @@ import (
 // global logger
 var log = logrus.New()
 
-var httpProcQuitDone = make(chan bool, 1)
-
-// status vars for webinterface
-var lastTransmittedSlaveConfig = "none yet"
-var lastTransmittedSlaveConfigTime time.Time
-
 var db *disttrace.DB
-
-func httpServer(accessLog string) {
-	var err error
-
-	log.Info("httpServer: Start...")
-
-	var accessWriter io.Writer
-	if accessWriter, err = os.OpenFile(accessLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
-		log.Panicf("httpServer: Can't open access log '%v', Error: %v", accessLog, err)
-	}
-
-	// handle slaves
-	slaveRouter := http.NewServeMux()
-	slaveRouter.HandleFunc("/slave/results", httpHandleSlaveResults())
-	slaveRouter.HandleFunc("/slave/config", httpHandleSlaveConfig())
-
-	// handle api requests from webinterface
-	authRouter := http.NewServeMux()
-	authRouter.HandleFunc("/api/status", httpHandleAPIStatus())
-	authRouter.HandleFunc("/api/traces", httpHandleAPITraceHistory())
-	authRouter.HandleFunc("/api/graph", httpHandleAPIGraphData())
-
-	authHandler := negroni.New()
-	authHandler.Use(negroni.HandlerFunc(checkJWTAuth))
-	authHandler.UseHandler(authRouter)
-
-	// handle everything else
-	rootRouter := http.NewServeMux()
-	rootRouter.HandleFunc("/", httpDefaultHandler())
-	rootRouter.HandleFunc("/api/auth", httpHandleAPIAuth())
-	rootRouter.Handle("/slave/", slaveRouter)
-	rootRouter.Handle("/api/", authHandler)
-
-	// register middleware for all requests
-	rootHandler := negroni.New()
-	rootHandler.Use(negroni.HandlerFunc(handleAccessControl))
-	rootHandler.Use(negroni.Wrap(ghandlers.CombinedLoggingHandler(accessWriter, rootRouter)))
-
-	srv := &http.Server{
-		Addr:    ":8990",
-		Handler: rootHandler,
-	}
-
-	// start server...
-	go func() {
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatal("httpServer: HTTP Server failure, ListenAndServe: ", err)
-		}
-	}()
-
-	// wait for quit signal...
-	for {
-		if disttrace.CheckForQuit() {
-			log.Warn("httpServer: Received signal to shutdown...")
-			ctx, cFunc := context.WithTimeout(context.Background(), 5*time.Second)
-			if err := srv.Shutdown(ctx); err != nil {
-				log.Warn("httpServer: Error while shutdown of HTTP server, Error: ", err)
-			}
-			cFunc()
-
-			log.Info("httpServer: Shutdown complete.")
-			httpProcQuitDone <- true
-			return
-		}
-
-		time.Sleep(1 * time.Second)
-	}
-}
 
 func main() {
 
@@ -122,16 +42,13 @@ func main() {
 		fSet.BoolVar(&sendHelp, "help", false, "display this message")
 		fSet.Parse(os.Args[1:])
 
-		var errMasterCfg, errTargetsCfg, errMainLog, errAccessLog, errDb error
+		var errMainLog, errAccessLog, errDb error
 		mainLogNameAndPath, errMainLog = disttrace.CleanAndCheckFileNameAndPath(mainLogNameAndPath)
 		accessLogNameAndPath, errAccessLog = disttrace.CleanAndCheckFileNameAndPath(accessLogNameAndPath)
 		dbNameAndPath, errDb = disttrace.CleanAndCheckFileNameAndPath(dbNameAndPath)
 
 		// valid cmdline arguments or exit
 		switch {
-		case errMasterCfg != nil || errTargetsCfg != nil:
-			log.Warn("Error: Invalid config file name, can't run, Bye.")
-			disttrace.PrintMasterUsageAndExit(fSet, true)
 		case errMainLog != nil || errAccessLog != nil:
 			log.Warn("Error: Invalid log path specified, can't run, Bye.")
 			disttrace.PrintMasterUsageAndExit(fSet, true)
---
+++ cmd/dist-traceroute-master/httpServer.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	ghandlers "github.com/gorilla/handlers"
+	"github.com/urfave/negroni"
+	"github.com/xmirakulix/dist-traceroute/disttrace"
+)
+
+var httpProcQuitDone = make(chan bool, 1)
+
+func httpServer(accessLog string) {
+	var err error
+
+	log.Info("httpServer: Start...")
+
+	var accessWriter io.Writer
+	if accessWriter, err = os.OpenFile(accessLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
+		log.Panicf("httpServer: Can't open access log '%v', Error: %v", accessLog, err)
+	}
+
+	// handle slaves
+	slaveRouter := http.NewServeMux()
+	slaveRouter.HandleFunc("/slave/results", httpHandleSlaveResults())
+	slaveRouter.HandleFunc("/slave/config", httpHandleSlaveConfig())
+
+	// handle api requests from webinterface
+	apiRouter := http.NewServeMux()
+	apiRouter.HandleFunc("/api/status", httpHandleAPIStatus())
+	apiRouter.HandleFunc("/api/traces", httpHandleAPITraceHistory())
+	apiRouter.HandleFunc("/api/graph", httpHandleAPIGraphData())
+	apiRouter.HandleFunc("/api/users", httpHandleAPIUsers())
+	apiRouter.HandleFunc("/api/slaves", httpHandleAPISlaves())
+	apiRouter.HandleFunc("/api/targets", httpHandleAPITargets())
+
+	authHandler := negroni.New()
+	authHandler.Use(negroni.HandlerFunc(checkJWTAuth))
+	authHandler.UseHandler(apiRouter)
+
+	// handle everything else
+	rootRouter := http.NewServeMux()
+	rootRouter.HandleFunc("/", httpDefaultHandler())
+	rootRouter.HandleFunc("/api/auth", httpHandleAPIAuth())
+	rootRouter.Handle("/slave/", slaveRouter)
+	rootRouter.Handle("/api/", authHandler)
+
+	// register middleware for all requests
+	rootHandler := negroni.New()
+	rootHandler.Use(negroni.HandlerFunc(handleAccessControl))
+	rootHandler.Use(negroni.Wrap(ghandlers.CombinedLoggingHandler(accessWriter, rootRouter)))
+
+	srv := &http.Server{
+		Addr:    ":8990",
+		Handler: rootHandler,
+	}
+
+	// start server...
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal("httpServer: HTTP Server failure, ListenAndServe: ", err)
+		}
+	}()
+
+	// wait for quit signal...
+	for {
+		if disttrace.CheckForQuit() {
+			log.Warn("httpServer: Received signal to shutdown...")
+			ctx, cFunc := context.WithTimeout(context.Background(), 5*time.Second)
+			if err := srv.Shutdown(ctx); err != nil {
+				log.Warn("httpServer: Error while shutdown of HTTP server, Error: ", err)
+			}
+			cFunc()
+
+			log.Info("httpServer: Shutdown complete.")
+			httpProcQuitDone <- true
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
